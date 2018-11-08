5582c08acdb0b9a91aa6ae91dc9a59174e8e4b22
Reto Stauffer <reto.stauffer@uibk.ac.at>
2018-11-08 07:29:44 +0100

    Small changes in theme

--- custom-news.php
+++ custom-news.php
@@ -12,61 +12,61 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header <?php print (is_single() ? "single-post" : "list-post"); ?>">
-		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
-		<div class="entry-meta">
-			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
-		</div>
-		<?php
-		endif;
+    <header class="entry-header <?php print (is_single() ? "single-post" : "list-post"); ?>">
+        <?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
+        <div class="entry-meta">
+            <span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
+        </div>
+        <?php
+        endif;
 
-	   custom_post_thumbnail(); 
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-		endif;
-		?>
+        custom_post_thumbnail(); 
+        if ( is_single() ) :
+            the_title( '<h1 class="entry-title">', '</h1>' );
+        else :
+            the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+        endif;
+        ?>
 
-		<div class="entry-meta">
-			<?php
-				if ( 'post' == get_post_type() )
-					twentyfourteen_posted_on();
+        <div class="entry-meta">
+            <?php
+                if ( 'post' == get_post_type() )
+                    twentyfourteen_posted_on();
 
-				if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
-			?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
-			<?php
-				endif;
+                if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+            ?>
+            <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
+            <?php
+                endif;
 
-				edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
-			?>
-		</div><!-- .entry-meta -->
+                edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
+            ?>
+        </div><!-- .entry-meta -->
 
-	<?php if ( is_search() ) : ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
-		<?php
-			/* translators: %s: Name of current post */
-			//the_content( sprintf(
-			//	__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
-			//	the_title( '<span class="screen-reader-text">', '</span>', false )
-			//) );
+    <?php if ( is_search() ) : ?>
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div><!-- .entry-summary -->
+    <?php else : ?>
+    <div class="entry-content">
+        <?php the_excerpt(); ?>
+        <?php
+            /* translators: %s: Name of current post */
+            //the_content( sprintf(
+            //  __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
+            //  the_title( '<span class="screen-reader-text">', '</span>', false )
+            //) );
 
-			//wp_link_pages( array(
-			//	'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-			//	'after'       => '</div>',
-			//	'link_before' => '<span>',
-			//	'link_after'  => '</span>',
-			//) );
-		?>
-	</div><!-- .entry-content -->
-	<?php endif; ?>
-	</header><!-- .entry-header -->
+            //wp_link_pages( array(
+            //  'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+            //  'after'       => '</div>',
+            //  'link_before' => '<span>',
+            //  'link_after'  => '</span>',
+            //) );
+        ?>
+    </div><!-- .entry-content -->
+    <?php endif; ?>
+    </header><!-- .entry-header -->
 
-	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+    <?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
 </article><!-- #post-## -->
